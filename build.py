ff7efba18fca0f8093e7ef89ab96952e0f0fd188
Talon <talontalon24@gmail.com>
2020-10-03 23:36:06 +0200

    Fix the autobuild

--- build.py
+++ build.py
@@ -14,8 +14,7 @@ def main():
             with zipfile.ZipFile(folder + '.zip', 'w') as myzip:
                 for file in os.listdir(folder):
                     if file.endswith(".py") or file == "UI_Config.json":
-                        print(file)
-                    myzip.write(os.path.join(os.getcwd(), folder, file))
+                        myzip.write(os.path.join(folder, file))
 
 
 if __name__ == '__main__':
