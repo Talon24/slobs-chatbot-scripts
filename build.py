2a5c4f5dc28c3b200c650a7dc48f058e10809144
Talon <talontalon24@gmail.com>
2020-10-05 22:00:56 +0200

    Include default settings in zipfiles

--- build.py
+++ build.py
@@ -2,31 +2,49 @@
 
 import os
 import re
+import json
 import zipfile
+import pathlib
 
 EXCLUDEDIRS = ["build"]
 
 
 def main():
     """Main"""
-    join = os.path.join
     try:
         os.mkdir("build")
     except FileExistsError:
         pass
     for folder in os.listdir():
-        if os.path.isdir(folder) and not folder.startswith(".") and folder not in EXCLUDEDIRS:
-            mainscript = [file for file in os.listdir(folder)
-                          if file.endswith("StreamlabsSystem.py")][0]
-            with open(join(folder, mainscript)) as readfile:
+        folder = pathlib.Path(folder)
+        if folder.is_dir() and not folder.name.startswith(".") and folder.name not in EXCLUDEDIRS:
+            mainscript = [file for file in folder.iterdir()
+                          if file.name.endswith("StreamlabsSystem.py")][0]
+            with open(mainscript) as readfile:
                 text = readfile.read()
             version = re.search(r"""Version = ["'](.+)["']""", text).group(1)
             zipname = "{}-{}.zip".format(folder, version)
             with zipfile.ZipFile(os.path.join("build", zipname), 'w') as myzip:
-                for file in os.listdir(folder):
-                    if file.endswith(".py") or file == "UI_Config.json":
-                        myzip.write(join(folder, file))
-                        print("File {} added to {}".format(file, zipname))
+                settings, settings_name = create_basic_settings(folder)
+                print("File {} added to {}".format(settings_name.name, zipname))
+                myzip.writestr(str(settings_name), settings)
+                for file in folder.iterdir():
+                    if file.suffix == ".py" or file.name == "UI_Config.json":
+                        myzip.write(file)
+                        print("File {} added to {}".format(file.name, zipname))
+
+
+def create_basic_settings(folder):
+    """Create initial settings.json file."""
+    with open(folder / "UI_Config.json") as readfile:
+        data = json.load(readfile)
+    outfile_name = folder / data["output_file"]
+    del data["output_file"]
+    outdata = {key: value["value"] for key, value in data.items()}
+    out = json.dumps(outdata, indent=4)
+    return out, outfile_name
+
+
 
 
 if __name__ == '__main__':
