87a368324ab13f3242034b85f086a35b0294b9ef
Talon <talontalon24@gmail.com>
2020-10-29 23:43:56 +0100

    Expand buildfile, now makes file with all scripts wit aggregated version

--- build.py
+++ build.py
@@ -11,6 +11,12 @@ EXCLUDEDIRS = ["build", ".git", ".github"]
 
 def main():
     """Main"""
+    generate_script_zips()
+    gather_all()
+
+
+def generate_script_zips():
+    """Make the script zipfiles, build the settings-json file from defaults."""
     try:
         os.mkdir("build")
     except FileExistsError:
@@ -23,6 +29,34 @@ def main():
             with zipfile.ZipFile(os.path.join("build", zipname), 'w') as myzip:
                 zip_content(folder, myzip)
                 zip_basic_settings(folder, myzip)
+
+
+def gather_all():
+    """Make a File with all the zips and the readme file."""
+    version = "0.0.0"
+    build = pathlib.Path("./build")
+    zips = []
+    for folder in pathlib.Path(".").iterdir():
+        if folder.is_dir() and folder.name not in EXCLUDEDIRS:
+            mainfile = find_mainfile(folder)
+            file_version = get_version(mainfile)
+            version = version_add(version, file_version)
+            zips.append(build / "{}-{}.zip".format(folder, file_version))
+    target = build / "All Scripts-{}.zip".format(version)
+    with zipfile.ZipFile(target, "w") as myzip:
+        print("Generating collection file {}".format(target))
+        for file in zips:
+            myzip.write(file)
+            print("Added to collection file: {}".format(file))
+        myzip.write("README.md")
+        myzip.write("LICENSE")
+
+
+def version_add(version1, version2):
+    """Add two tuples elementwise."""
+    version = [int(first) + int(second) for first, second
+               in zip(version1.split("."), version2.split("."))]
+    return "{}.{}.{}".format(*version)
 
 
 def find_mainfile(folder):
@@ -48,7 +82,7 @@ def zip_content(folder, myzip):
     for file in folder.iterdir():
         if file.suffix == ".py" or file.name == "UI_Config.json":
             myzip.write(file)
-            print("File {} added to {}".format(file.name, myzip.filename))
+            print("In {} this was added: {}".format(myzip.filename, file.name))
 
 
 def zip_basic_settings(folder, myzip):
@@ -64,7 +98,7 @@ def zip_basic_settings(folder, myzip):
     outdata = {key: value["value"] for key, value in data.items() if "value" in value}
     settings = json.dumps(outdata, indent=4, ensure_ascii=False)
     myzip.writestr(str(settings_name), settings)
-    print("File {} added to {}".format(settings_name.name, myzip.filename))
+    print("In {} this was added: {}".format(myzip.filename, settings_name.name))
 
 
 if __name__ == '__main__':
