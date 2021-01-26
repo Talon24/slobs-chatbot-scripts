1d4df768c1d116084f51a0c4ec236087d0483f34
Talon <talontalon24@gmail.com>
2021-01-27 00:08:14 +0100

    Streamline the viewbot hunter

--- Viewbot Hunter/bot_hunter_StreamlabsSystem.py
+++ Viewbot Hunter/bot_hunter_StreamlabsSystem.py
@@ -17,7 +17,7 @@ ScriptName = "Viewbot Hunter"
 Website = "https://github.com/Talon24"
 Description = "Check for bots and block them"
 Creator = "Talon24"
-Version = "0.9.1"
+Version = "0.9.2"
 
 # Have pylint know the parent variable
 if False:  # pylint: disable=using-constant-test
@@ -88,9 +88,11 @@ def update_botlist():
         else:
             log("Error when getting botlist!\n"
                 "stdout {}, stderr {}, code {}".format(stdout, stderr, code))
+        return False
     else:
         BOTS.update(json.loads(stdout))
         serialize_botlist()
+        return True
 
 
 def ReloadSettings(_jsonData):
@@ -208,22 +210,79 @@ def install_selenium():
         status = "Success" if not output[2] else "Failure"
         log("getpip output: {}\n{}\n{}".format(status, output[0], output[1]))
     except NameError:
-        # Attempt to get around missing PROTOCOL_TLS in ssl module error?
-        # I guess it means it found pip but ironpython did its thing?
-        log("If you see this message then I found the PROTOCOL_TLS error, "
-            "please let me know of that! :)")
+        # pip is installed, but due to ironPython it will raise this error.
+        # Not an issue as it's called outside of it.
+        log("Pip is installed.")
     # Call pip to install selenium
-    command = ["-m", "pip", "install", "selenium"]
-    log("call: {}".format(" ".join(command)))
+    command = ["-m", "pip", "install", "--upgrade", "selenium"]
+    # log("call: {}".format(" ".join(command)))
     output = call_python(command)
-    log("selenium install output: {}\n{}\n{}".format(
-        not bool(output[2]), output[0], output[1]))
+    if bool(output[2]):
+        log("Selenium install output: {}\n{}\n{}".format(
+            not bool(output[2]), output[0], output[1]))
+    else:
+        log("Selenium is installed.")
+
+
+def self_check():
+    """Check if everything works, and log status."""
+    # pylint: disable=too-many-branches
+    status = {}
+    try:
+        import pip  # pylint: disable=import-outside-toplevel, unused-import
+    except ImportError:
+        status["Pip"] = "Failure"
+    except NameError:
+        status["Pip"] = "Success"
+    else:
+        status["Pip"] = "Success"
+    try:
+        import selenium  # pylint: disable=import-outside-toplevel, unused-import
+    except ImportError:
+        status["Selenium"] = "Failure"
+    else:
+        status["Selenium"] = "Success"
+    if not update_botlist():
+        status["Botlist Updater"] = "Failure"
+    else:
+        status["Botlist Updater"] = "Success"
+    messages = ["Status report:"]
+    for key, value in status.items():
+        messages.append("{}: {}".format(key, value))
+    messages.append("Known bots: {}".format(len(BOTS)))
+    messages.append("Banned bots: {}".format(len(BANNED)))
+    templates = get_templates()
+    messages.append("Message templates: {}".format(len(templates)))
+    missing_placeholders = [template for template in templates
+                            if "{}" not in template]
+    for template in missing_placeholders:
+        messages.append("The message >>{}<< is missing the paceholder {{}}, "
+                        "so the bot name will not appear.".format(template))
+    if missing_placeholders:
+        status["Message Templates"] = "Failure"
+    else:
+        status["Message Templates"] = "Success"
+    if all([stat == "Success" for stat in status.values()]):
+        messages.append("Everything is set up correctly!")
+    log("\n".join(messages))
+    if status["Selenium"] == "Failure":
+        log("Required Module not installed, please click the "
+            "\"Install Selenium\" button.")
+    elif status["Botlist Updater"] == "Failure":
+        log("The webdriver is missing! Please refer to the table at "
+            "https://github.com/Talon24/slobs-chatbot-scripts")
 
 
 def open_explorer():
     """Open the windows explorer at the scripts directory."""
     workdir = os.path.dirname(__file__)
     call(["explorer.exe", workdir], hidden=True)
+
+
+def open_templates():
+    """Open the windows explorer at the scripts directory."""
+    file = os.path.join(os.path.dirname(__file__), "message_templates.txt")
+    call([r"notepad.exe", file], hidden=True, waiting=False)
 
 
 def call_python(command, hidden=False):
@@ -235,7 +294,7 @@ def call_python(command, hidden=False):
     return stdout, stderr, code
 
 
-def call(command, hidden=False):
+def call(command, hidden=False, waiting=True):
     """Make a commandline call."""
     pipe = subprocess.PIPE
     flags = 0
@@ -243,6 +302,8 @@ def call(command, hidden=False):
         flags |= 0x00000008  # detached process flag, hides terminal
     term = subprocess.Popen(command, stdin=pipe, stdout=pipe, stderr=pipe,
                             creationflags=flags)
+    if not waiting:
+        return None, None, None
     stdout, stderr = term.communicate(b"")
     code = term.returncode
     try:
